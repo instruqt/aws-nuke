655e82cad6f12755bdb7fca859574b861db4771d
Vitalii Kozlovskyi <vitalii@kozlovskyi.dev>
2023-11-06 01:09:15 +0100

    Move mandatory forcetime cooldown check to cobra parameter validation, so it's possible to skip it, when using AWS-Nuke as Library, invoking .Run method.

--- cmd/nuke.go
+++ cmd/nuke.go
@@ -32,13 +32,9 @@ func NewNuke(params NukeParameters, account awsutil.Account) *Nuke {
 
 func (n *Nuke) Run() error {
 	var err error
-
-	if n.Parameters.ForceSleep < 3 && n.Parameters.NoDryRun {
-		return fmt.Errorf("Value for --force-sleep cannot be less than 3 seconds if --no-dry-run is set. This is for your own protection.")
-	}
-	forceSleep := time.Duration(n.Parameters.ForceSleep) * time.Second
-
 	fmt.Printf("aws-nuke version %s - %s - %s\n\n", BuildVersion, BuildDate, BuildHash)
+
+	forceSleep := time.Duration(n.Parameters.ForceSleep) * time.Second
 
 	err = n.Config.ValidateAccount(n.Account.ID(), n.Account.Aliases())
 	if err != nil {
--- cmd/params.go
+++ cmd/params.go
@@ -24,6 +24,9 @@ func (p *NukeParameters) Validate() error {
 	if strings.TrimSpace(p.ConfigPath) == "" {
 		return fmt.Errorf("You have to specify the --config flag.\n")
 	}
+	if p.ForceSleep < 3 && p.NoDryRun {
+		return fmt.Errorf("Value for --force-sleep cannot be less than 3 seconds if --no-dry-run is set. This is for your own protection.\n")
+	}
 
 	return nil
 }
